1a88a64575810ab2c68eb107472dd38bae73bdc5
LiamPKenna <liampkenna@gmail.com>
2020-02-28 10:23:58 -0800

    Add component for NavBar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Lesson from './Lesson'
+import Lesson from './Lesson';
+import NavBar from './NavBar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       HELLO!
+       <NavBar />
       </header>
       <Lesson />
     </div>
