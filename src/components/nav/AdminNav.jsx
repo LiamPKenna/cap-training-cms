7570876029b4904f0d7a15a0ab74d602db302907
LiamPKenna <liampkenna@gmail.com>
2020-02-28 14:03:28 -0800

    Style nav and display logo if available

--- src/components/nav/AdminNav.jsx
+++ src/components/nav/AdminNav.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-const AdminNav = () => {
-  return <div>Admin Nav Bar</div>;
+const AdminNav = props => {
+  const navStyle = {
+    height: "75px",
+    objectFit: "contain",
+    border: "1px solid grey"
+  };
+  const imgStyle = {
+    height: "50px",
+    margin: "10px"
+  };
+  const h2Style = {
+    margin: "25px 10px"
+  };
+  const makeHero = () => {
+    return props.logo ? (
+      <img style={imgStyle} src={props.logo} alt={`${props.brand} logo`} />
+    ) : (
+      <h2 style={h2Style}>{props.brand}</h2>
+    );
+  };
+
+  return <div style={navStyle}>{makeHero()}</div>;
 };
 
 export default AdminNav;
