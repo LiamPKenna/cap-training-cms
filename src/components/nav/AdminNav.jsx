85ac8ec7fbb35c70a59b858e8f01b6c87acab3c8
LiamPKenna <liampkenna@gmail.com>
2020-02-28 14:25:36 -0800

    Add Material drawer menu

--- src/components/nav/AdminNav.jsx
+++ src/components/nav/AdminNav.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 const AdminNav = props => {
+  const [drawer, setDrawer] = useState(false);
+
   const navStyle = {
     height: "75px",
     objectFit: "contain",
-    border: "1px solid grey"
+    border: "1px solid grey",
+    display: "grid",
+    gridTemplateColumns: "1fr 100px",
+    width: "100%"
+  };
+  const drawerStyle = {
+    position: "fixed",
+    right: "0",
+    left: "auto",
+    width: "150px",
+    boxSizing: "inherit",
+    background: "#545454",
+    display: "block"
   };
   const imgStyle = {
     height: "50px",
@@ -21,7 +40,38 @@ const AdminNav = props => {
     );
   };
 
-  return <div style={navStyle}>{makeHero()}</div>;
+  const toggleDrawer = (side, open) => event => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setDrawer(!drawer);
+  };
+
+  return (
+    <div>
+      <div style={navStyle}>
+        {makeHero()}
+        <Button onClick={toggleDrawer("right", true)}>=</Button>
+      </div>
+      <Drawer
+        style={drawerStyle}
+        anchor="right"
+        open={drawer}
+        onClose={() => setDrawer(false)}
+      >
+        <List>
+          {["Inbox", "Starred", "Send email", "Drafts"].map(text => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </div>
+  );
 };
 
 export default AdminNav;
