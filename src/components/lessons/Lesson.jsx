2ee03ca5d0696bade10b93514dbf5de09e392862
LiamPKenna <liampkenna@gmail.com>
2020-03-01 05:44:17 -0800

    Add Container component

--- src/components/lessons/Lesson.jsx
+++ src/components/lessons/Lesson.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import makeSection from "./makeSection";
 import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
+import Container from "@material-ui/core/Container";
 
 const makePage = lesson => lesson.map((l, i) => makeSection(l, i));
 
 const Lesson = props => {
   const { courseId, segmentId, lessonId } = useParams();
   const lesson = props.courses[courseId].segments[segmentId].lessons[lessonId];
-  return <div>{makePage(lesson.content)}</div>;
+  return <Container>{makePage(lesson.content)}</Container>;
 };
 
 const mapStateToProps = state => {
