5823dcf5e407d486879db4dbe3590b3d5e756656
LiamPKenna <liampkenna@gmail.com>
2020-03-02 06:56:47 -0800

    Attempt to setup first push to db

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,15 @@
 import constants from './../constants';
-import Firebase from 'firebase';
+import firebase from 'firebase';
 const { firebaseConfig } = constants;
 
-Firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database().ref('training');
+
+// db.ref('/brand').set({
+//   name: "Epicodus",
+//   logo: "https://static1.squarespace.com/static/5524448ee4b0d6f6b83ab9e2/t/57cf3de246c3c4d2933aa57c/1529949737992/?format=1500w"
+
+// });
+
+export default db;
