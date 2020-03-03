0692eb3acb2ecd7b260ef45b6f70479b76f3e406
LiamPKenna <liampkenna@gmail.com>
2020-03-03 05:51:01 -0800

    Add initial action types

---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,10 @@
+export const ADD_ELEMENT = 'ADD_ELEMENT';
+export const ADD_COURSE = 'ADD_COURSE';
+export const ADD_LESSON = 'ADD_LESSON';
+export const REMOVE_ELEMENT = 'REMOVE_ELEMENT';
+export const REMOVE_LESSON = 'REMOVE_LESSON';
+export const REMOVE_COURSE = 'REMOVE_COURSE';
+export const SHIFT_ELEMENT = 'SHIFT_ELEMENT';
+export const UPDATE_ELEMENT = 'UPDATE_ELEMENT';
+export const UPDATE_COURSE = 'UPDATE_COURSE';
+
