bd6b0571fc3d5372037229f7c691527e5c0edac7
LiamPKenna <liampkenna@gmail.com>
2020-03-03 15:15:36 -0800

    Update action types

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -3,9 +3,11 @@ export const ADD_COURSE = 'ADD_COURSE';
 export const ADD_LESSON = 'ADD_LESSON';
 export const REMOVE_ELEMENT = 'REMOVE_ELEMENT';
 export const REMOVE_LESSON = 'REMOVE_LESSON';
+export const RECEIVE_LESSON = 'RECEIVE_LESSON';
+export const RECEIVE_COURSE = 'RECEIVE_COURSE';
 export const REMOVE_COURSE = 'REMOVE_COURSE';
 export const SHIFT_ELEMENT = 'SHIFT_ELEMENT';
 export const UPDATE_ELEMENT = 'UPDATE_ELEMENT';
 export const UPDATE_COURSE = 'UPDATE_COURSE';
-export const GET_ALL_LESSONS = 'GET_ALL_LESSONS';
+export const UPDATE_TEXT = 'UPDATE_TEXT';
 
