a3c01d9f5f66339763fbb05b5bfc1f7725108ada
LiamPKenna <liampkenna@gmail.com>
2020-03-03 15:12:55 -0800

    Add new Loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Loading = () => {
+  const loadStyle = {
+    paddingTop: "6rem",
+    textAlign: "center"
+  };
+  return (
+    <div style={loadStyle}>
+      <h2>LOADING...</h2>
+    </div>
+  );
+};
+
+export default Loading;
