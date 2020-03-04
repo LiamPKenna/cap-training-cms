462668a0677bc06b8914f73a1a8cd86645af92f7
LiamPKenna <liampkenna@gmail.com>
2020-03-04 14:14:22 -0800

    Fix name

--- src/components/courses/NewItemInput.jsx
+++ src/components/courses/NewItemInput.jsx
@@ -5,7 +5,7 @@ import Button from "@material-ui/core/Button";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import CancelIcon from "@material-ui/icons/Cancel";
 
-const NewCourseInput = props => {
+const NewItemInput = props => {
   const mainGridStyle = {
     display: "grid",
     gridTemplateColumns: "70px 1fr 70px",
@@ -33,4 +33,4 @@ const NewCourseInput = props => {
   );
 };
 
-export default NewCourseInput;
+export default NewItemInput;
