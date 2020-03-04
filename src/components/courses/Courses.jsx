762d034570bddea0885017705d073762472e6e65
LiamPKenna <liampkenna@gmail.com>
2020-03-04 13:51:21 -0800

    Refactor and add handlers for new course input

--- src/components/courses/Courses.jsx
+++ src/components/courses/Courses.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Loading from "../Loading";
 import { addCourse } from "../../actions";
+import NewCourseButton from "./NewCourseButton";
+import NewCourseInput from "./NewCourseInput";
 
 const Courses = props => {
+  const [showForm, setShowForm] = useState(false);
+  const [newCourseText, setNewCourseText] = useState("");
+
+  const showNewCourseForm = () => {
+    setShowForm(true);
+  };
+
+  const handleNewCourse = () => {
+    setShowForm(false);
+    props.dispatch(addCourse(newCourseText));
+  };
+
+  const cancelInput = () => {
+    setShowForm(false);
+    setNewCourseText("");
+  };
+
   const makeCourses = () => {
     if (!props.courses) {
       return <Loading />;
@@ -21,7 +40,18 @@ const Courses = props => {
   return (
     <div>
       <ul>{makeCourses()}</ul>
-      <button onClick={() => props.dispatch(addCourse())}>NEW</button>
+      {!showForm ? <NewCourseButton clickHandler={showNewCourseForm} /> : ""}
+      {showForm ? (
+        <NewCourseInput
+          value={newCourseText}
+          handleChange={e => setNewCourseText(e.target.value)}
+          handleSubmit={handleNewCourse}
+          inputName="Course Name"
+          handleCancel={cancelInput}
+        />
+      ) : (
+        ""
+      )}
     </div>
   );
 };
