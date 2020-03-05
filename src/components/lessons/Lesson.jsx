07f0502c303a108f210365422f1e14caa6b28cfa
LiamPKenna <liampkenna@gmail.com>
2020-03-05 09:49:15 -0800

    Add handling for new elements

--- src/components/lessons/Lesson.jsx
+++ src/components/lessons/Lesson.jsx
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
 import Loading from "../Loading";
 import NewElementSelector from "./NewElementSelector";
+import { createElement } from "../../actions";
 
 const makePage = (lesson, lessonId) =>
   lesson.map((e, i) => makeElement(e, i, lessonId));
@@ -11,10 +12,13 @@ const makePage = (lesson, lessonId) =>
 const Lesson = props => {
   const { lessonId } = useParams();
   const lesson = props.lessons[lessonId];
+  const addNewElement = newElementType => {
+    createElement(newElementType, lesson.content, lesson.lessonId);
+  };
   return lesson ? (
     <div>
       {makePage(lesson.content, lessonId)}
-      {true ? <NewElementSelector /> : ""}
+      {true ? <NewElementSelector handleNewElement={addNewElement} /> : ""}
     </div>
   ) : (
     <Loading />
