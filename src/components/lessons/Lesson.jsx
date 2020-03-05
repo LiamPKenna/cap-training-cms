e6190df3b88a8eed887cbf4d0ee51a66aa024e52
LiamPKenna <liampkenna@gmail.com>
2020-03-05 09:26:46 -0800

    Add new element selector component

--- src/components/lessons/Lesson.jsx
+++ src/components/lessons/Lesson.jsx
@@ -3,6 +3,7 @@ import makeElement from "./makeElement";
 import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
 import Loading from "../Loading";
+import NewElementSelector from "./NewElementSelector";
 
 const makePage = (lesson, lessonId) =>
   lesson.map((e, i) => makeElement(e, i, lessonId));
@@ -10,7 +11,14 @@ const makePage = (lesson, lessonId) =>
 const Lesson = props => {
   const { lessonId } = useParams();
   const lesson = props.lessons[lessonId];
-  return lesson ? <div>{makePage(lesson.content, lessonId)}</div> : <Loading />;
+  return lesson ? (
+    <div>
+      {makePage(lesson.content, lessonId)}
+      {true ? <NewElementSelector /> : ""}
+    </div>
+  ) : (
+    <Loading />
+  );
 };
 
 const mapStateToProps = state => {
---
+++ src/components/lessons/NewElementSelector.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+
+const NewElementSelector = props => {
+  const [value, setValue] = useState("text");
+
+  const handleChange = event => {
+    setValue(event.target.value);
+  };
+
+  const divGridStyle = {
+    display: "grid",
+    gridTemplateColumns: "70px 1fr",
+    gridColumnGap: "15px"
+  };
+  const labelStyle = {
+    textAlign: "center",
+    margin: "0",
+    padding: "10px 0 10px 0",
+    borderBottom: "1px solid lightgrey"
+  };
+  const newElementStyle = {
+    backgroundColor: "#f5f5f5"
+  };
+  return (
+    <Paper component="form">
+      <div style={newElementStyle}>
+        <h6 style={labelStyle}>ADD A NEW ELEMENT</h6>
+        <div style={divGridStyle}>
+          <Button color="primary" onClick={() => props.handleNewElement(value)}>
+            <CheckCircleIcon />
+          </Button>
+          <RadioGroup
+            aria-label="element type"
+            name="element type"
+            value={value}
+            onChange={handleChange}
+            row
+          >
+            <FormControlLabel
+              value="text"
+              control={<Radio color="primary" />}
+              label="Text"
+            />
+            <FormControlLabel
+              value="title"
+              control={<Radio color="primary" />}
+              label="Title"
+            />
+            <FormControlLabel
+              value="heading"
+              control={<Radio color="primary" />}
+              label="Heading"
+            />
+            <FormControlLabel
+              value="subHeading"
+              control={<Radio color="primary" />}
+              label="Sub Heading"
+            />
+            <FormControlLabel
+              value="code"
+              control={<Radio color="primary" />}
+              label="Code"
+            />
+            <FormControlLabel
+              value="picture"
+              control={<Radio color="primary" />}
+              label="Picture"
+            />
+          </RadioGroup>
+        </div>
+      </div>
+    </Paper>
+  );
+};
+
+export default NewElementSelector;
