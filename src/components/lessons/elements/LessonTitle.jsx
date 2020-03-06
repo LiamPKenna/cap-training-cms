ab688aaad7a751aaf0812f1111d77e5189eb63d8
LiamPKenna <liampkenna@gmail.com>
2020-03-06 10:33:03 -0800

    Update title styles

--- src/components/lessons/elements/LessonTitle.jsx
+++ src/components/lessons/elements/LessonTitle.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const LessonTitle = params => {
   const { format, content } = params;
   const titleStyle = {
+    margin: "2rem 0 0.5rem",
     textAlign: format.align
   };
   return <h1 style={titleStyle}>{content}</h1>;
