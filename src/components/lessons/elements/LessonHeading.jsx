4c2430a4a0e463fabb3df2778917a6c6fa6ca092
LiamPKenna <liampkenna@gmail.com>
2020-03-06 10:05:24 -0800

    Convert to styled components

--- src/components/lessons/elements/LessonHeading.jsx
+++ src/components/lessons/elements/LessonHeading.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styled from "styled-components";
+
+const LessonHeadingElement = styled.h2`
+  margin: 1rem 0;
+`;
 
 const LessonHeading = params => {
-  return <h2>{params.content}</h2>;
+  return <LessonHeadingElement>{params.content}</LessonHeadingElement>;
 };
 
 export default LessonHeading;
