993f8d60f28c056892fc6646f03e6c657792d163
LiamPKenna <liampkenna@gmail.com>
2020-03-06 10:24:41 -0800

    Convert to styled components

--- src/components/lessons/elements/LessonSub.jsx
+++ src/components/lessons/elements/LessonSub.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import styled from "styled-components";
+
+const SubElement = styled.h4`
+  margin: 0.5rem 0 0;
+`;
+
+const StyledHr = styled.hr`
+  background: linear-gradient(
+    to right,
+    rgba(230, 230, 230, 1) 20%,
+    rgba(236, 236, 236, 0) 80%
+  );
+  height: 1px;
+  border: none;
+  margin-bottom: 0.5rem;
+`;
 
 const LessonSub = params => {
-  return <h4>{params.content}</h4>;
+  return (
+    <>
+      <SubElement>{params.content}</SubElement>
+      <StyledHr />
+    </>
+  );
 };
 
 export default LessonSub;
