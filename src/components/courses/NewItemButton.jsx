40cfe9988aad5c1575e05d57017261d09d3f4fea
LiamPKenna <liampkenna@gmail.com>
2020-03-06 11:06:38 -0800

    Convert to styled components

--- src/components/courses/NewItemButton.jsx
+++ src/components/courses/NewItemButton.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
+import styled from "styled-components";
+
+const PlusButton = styled.button`
+  border-radius: 50%;
+  border: none;
+  background-color: rgba(0, 0, 0, 0);
+  color: grey;
+  margin: 0.25rem;
+`;
 
 const NewItemButton = props => {
-  const buttonStyle = {
-    borderRadius: "50%",
-    border: "none",
-    backgroundColor: "rgba(0,0,0,0)",
-    color: "grey",
-    margin: "0.25rem .5rem"
-  };
   return (
-    <button
-      style={buttonStyle}
-      onClick={props.clickHandler}
-      title={props.title}
-    >
+    <PlusButton onClick={props.clickHandler} title={props.title}>
       <AddCircleIcon fontSize={"large"} />
-    </button>
+    </PlusButton>
   );
 };
 
