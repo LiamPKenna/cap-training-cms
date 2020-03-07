236ee1a8e08326168741d725df40155c57c95ff1
LiamPKenna <liampkenna@gmail.com>
2020-03-07 12:25:06 -0800

    Clean-up auth config

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,15 +3,9 @@ import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import firebase from "firebase";
 
 const uiConfig = {
-  // Popup signin flow rather than redirect flow.
   signInFlow: "popup",
-  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: "/signedIn",
-  // We will display Google and Facebook as auth providers.
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID
-  ]
+  signInSuccessUrl: "/courses",
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID]
 };
 
 const Home = () => {
