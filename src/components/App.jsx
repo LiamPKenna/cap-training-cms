ea8fa36e21b030cb86e4fd36a056292e1da25082
LiamPKenna <liampkenna@gmail.com>
2020-03-08 10:11:40 -0700

    Show login screen if no user, watch auth state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Lesson from "./lessons/Lesson";
-import VideoLesson from "./lessons/VideoLesson";
 import Courses from "./courses/Courses";
 import Course from "./courses/Course";
 import NavBar from "./nav/NavBar";
@@ -9,38 +8,60 @@ import Container from "@material-ui/core/Container";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { watchFirebaseLessonsRef, watchFirebaseCoursesRef } from "../actions";
 import { useDispatch } from "react-redux";
+import SignIn from "./SignIn";
+import { auth } from "../firebase";
 
 function App(props) {
   const dispatch = useDispatch();
+  const [currentUser, setCurrentUser] = useState(null);
 
+  auth.onAuthStateChanged(function(user) {
+    if (user) {
+      setCurrentUser(user);
+      dispatch({
+        type: "SET_ADMIN",
+        admin: user.email === "liam@liamkenna.com"
+      });
+    } else {
+      setCurrentUser(null);
+      dispatch({ type: "SET_ADMIN", admin: false });
+    }
+  });
   useEffect(() => {
     dispatch(watchFirebaseLessonsRef());
     dispatch(watchFirebaseCoursesRef());
   });
+
   return (
     <div className="App">
       <Router>
         <header className="App-header">
-          <NavBar admin={true} />
+          <NavBar user={currentUser} />
         </header>
         <Container>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/courses">
-              <Courses courses={props.courses} />
-            </Route>
-            <Route path="/courses/:courseId">
-              <Course />
-            </Route>
-            <Route path="/lessons/:lessonId">
-              <Lesson />
-            </Route>
-            <Route path="/video_lessons/:lessonId">
-              <VideoLesson />
-            </Route>
-          </Switch>
+          {currentUser ? (
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/courses">
+                <Courses courses={props.courses} />
+              </Route>
+              <Route path="/courses/:courseId">
+                <Course />
+              </Route>
+              <Route path="/lessons/:lessonId">
+                <Lesson />
+              </Route>
+              <Route path="/signin">
+                <>
+                  <SignIn />
+                </>
+              </Route>
+            </Switch>
+          ) : (
+            <SignIn />
+          )}
         </Container>
       </Router>
     </div>
