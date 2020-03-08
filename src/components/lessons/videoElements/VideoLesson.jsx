737493145d26f98c00de4cdbf729498a76ac8f49
LiamPKenna <liampkenna@gmail.com>
2020-03-08 13:28:12 -0700

    Move unused video components

--- src/components/lessons/videoElements/VideoLesson.jsx
+++ src/components/lessons/videoElements/VideoLesson.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-import makeElement from "./makeElement";
 import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
 import Loading from "../Loading";
 import NewElementSelector from "./NewElementSelector";
 import { createElement } from "../../actions";
-import VideoUpload from "../lessons/VideoUpload";
+import VideoUpload from "./videoElements/VideoUpload";
+import VideoBlock from "./videoElements/VideoBlock";
 
 const makePage = (lesson, lessonId) => {
-  if (lesson.video) {
-    return lesson.map((e, i) => makeElement(e, i, lessonId, lesson));
+  if (lesson) {
+    return (
+      <VideoBlock
+        src={"https://www.youtube-nocookie.com/embed/JQjroW6J4mw"}
+        title={"test"}
+      />
+      // <div>
+      //   {/* {lesson.map((e, i) => makeElement(e, i, lessonId, lesson))} */}
+      // </div>
+    );
   } else {
     return (
       <>
--- src/components/lessons/videoElements/VideoUpload.jsx
+++ src/components/lessons/videoElements/VideoUpload.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import Button from "@material-ui/core/Button";
-import { storage } from "../../firebase";
+import { storage } from "../../../firebase";
 
 const VideoInputDiv = styled.div`
   display: flex;
