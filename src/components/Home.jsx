216198f73c05056a773a01c8ea66589a0cffade3
LiamPKenna <liampkenna@gmail.com>
2020-03-08 10:12:20 -0700

    Return to simple Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-import { ui, uiConfig } from "../firebase";
 
 const Home = () => {
   return (
     <div>
       <h1>Hi</h1>
       <h4>This is the home page</h4>
-      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={ui} />
     </div>
   );
 };
