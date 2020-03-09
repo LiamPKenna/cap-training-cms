dec59903a33f9c3942ba5b4c48b63be8cfe0b53c
LiamPKenna <liampkenna@gmail.com>
2020-03-09 10:56:11 -0700

    Refactor for clarity

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,68 +1,76 @@
-import constants from './../constants';
-import { db, auth } from '../firebase';
+import constants from "./../constants";
+import { db, auth } from "../firebase";
 const { c } = constants;
 
-
 export function watchFirebaseLessonsRef() {
-  return (dispatch) => {
-    db.ref('/lessons').on('child_added', params => {
+  return dispatch => {
+    db.ref("/lessons").on("child_added", params => {
       const newLesson = Object.assign({}, params.val());
       dispatch(receiveLesson(newLesson));
     });
   };
 }
 
-export const receiveLesson = (lessonFromFirebase) => {
+export const receiveLesson = lessonFromFirebase => {
   return {
     type: c.RECEIVE_LESSON,
     lesson: lessonFromFirebase
   };
-}
+};
 
 export function watchFirebaseCoursesRef() {
-  return (dispatch) => {
-    db.ref('/courses').on('child_added', params => {
+  return dispatch => {
+    db.ref("/courses").on("child_added", params => {
       const newCourse = Object.assign({}, params.val());
       dispatch(receiveCourse(newCourse));
     });
   };
 }
 
-export const receiveCourse = (courseFromFirebase) => {
+export const receiveCourse = courseFromFirebase => {
   return {
     type: c.RECEIVE_COURSE,
     course: courseFromFirebase
   };
-}
+};
 
 export const signOut = () => {
   auth.signOut();
-}
+};
 
-export const updateText = (params) => {
+export const updateText = params => {
   var updates = {};
-  updates['/lessons/' + params.lessonId + '/content/' + params.elementIndex + '/content'] = params.content;
+  updates[
+    "/lessons/" +
+      params.lessonId +
+      "/content/" +
+      params.elementIndex +
+      "/content"
+  ] = params.content;
   db.ref().update(updates);
   return {
     type: c.UPDATE_TEXT,
     elementIndex: params.elementIndex,
     content: params.content,
     lessonId: params.lessonId
-  }
+  };
 };
 
 export const addCourse = (courseTitle = "New Course") => {
-  const newCourseKey = db.ref().child('courses').push().key;
+  const newCourseKey = db
+    .ref()
+    .child("courses")
+    .push().key;
   var updates = {};
-  updates['/courses/' + newCourseKey] = {
-    "segments": {
-      'default': {
-        title: 'First Segment',
-        lessons: { 'placeholder': { lessonId: 1, title: "TestLesson" } }
+  updates["/courses/" + newCourseKey] = {
+    segments: {
+      default: {
+        title: "First Segment",
+        lessons: { placeholder: { lessonId: 1, title: "TestLesson" } }
       }
     },
-    "title": courseTitle,
-    "id": newCourseKey
+    title: courseTitle,
+    id: newCourseKey
   };
   db.ref().update(updates);
   return {
@@ -73,12 +81,14 @@ export const addCourse = (courseTitle = "New Course") => {
 };
 
 export const addSegment = async (segmentTitle = "New segment", courseId) => {
-
-  const newSegmentKey = db.ref('/courses/' + courseId).child('segments').push().key;
+  const newSegmentKey = db
+    .ref("/courses/" + courseId)
+    .child("segments")
+    .push().key;
   var updates = {};
-  updates['/courses/' + courseId + '/segments/' + newSegmentKey] = {
-    "lessons": [{ lessonId: 0, title: "default" }],
-    "title": segmentTitle,
+  updates["/courses/" + courseId + "/segments/" + newSegmentKey] = {
+    lessons: [{ lessonId: 0, title: "default" }],
+    title: segmentTitle
   };
 
   const ret = await db.ref().update(updates);
@@ -93,56 +103,74 @@ export const addSegment = async (segmentTitle = "New segment", courseId) => {
   };
 };
 
-export const newLesson = (lessonTitle = "New Lesson", courseId, segmentId, oldLessons) => {
-  const newLessonKey = db.ref().child('lessons').push().key;
+export const newLesson = (
+  lessonTitle = "New Lesson",
+  courseId,
+  segmentId,
+  oldLessons
+) => {
+  const newLessonKey = db
+    .ref()
+    .child("lessons")
+    .push().key;
   var updates = {};
-  updates['/lessons/' + newLessonKey] = {
+  updates["/lessons/" + newLessonKey] = {
     title: lessonTitle,
     lessonId: newLessonKey,
     courseId,
     segmentId,
-    content: [{ type: 'default' }]
+    content: [{ type: "default" }]
   };
-  updates['/courses/' + courseId + '/segments/' + segmentId + '/lessons'] = [...oldLessons, {
-    title: lessonTitle,
-    lessonId: newLessonKey
-  }];
+  updates["/courses/" + courseId + "/segments/" + segmentId + "/lessons"] = [
+    ...oldLessons,
+    {
+      title: lessonTitle,
+      lessonId: newLessonKey
+    }
+  ];
   db.ref().update(updates);
-  return [{
-    type: c.ADD_LESSON,
-    title: lessonTitle,
-    lessonId: newLessonKey,
-    courseId,
-    segmentId
-  }, {
-    type: c.ADD_LESSON_TO_SEGMENT, title: lessonTitle,
-    lessonId: newLessonKey,
-    courseId,
-    segmentId
-  }]
+  return [
+    {
+      type: c.ADD_LESSON,
+      title: lessonTitle,
+      lessonId: newLessonKey,
+      courseId,
+      segmentId
+    },
+    {
+      type: c.ADD_LESSON_TO_SEGMENT,
+      title: lessonTitle,
+      lessonId: newLessonKey,
+      courseId,
+      segmentId
+    }
+  ];
 };
 
 export const createElement = (type, oldContent, lessonId) => {
-  const newElement = (type === 'break') ? { type } : {
-    type,
-    content: `This is a new ${type.toUpperCase()} element`,
-    format: {
-      align: "left"
-    }
-  };
+  const newElement =
+    type === "break"
+      ? { type }
+      : {
+          type,
+          content: `This is a new ${type.toUpperCase()} element`,
+          format: {
+            align: "left"
+          }
+        };
   var updates = {};
-  updates['/lessons/' + lessonId + '/content'] = [...oldContent, newElement];
+  updates["/lessons/" + lessonId + "/content"] = [...oldContent, newElement];
   db.ref().update(updates);
   return {
     type: c.ADD_ELEMENT,
     lessonId,
     newElement
-  }
-}
+  };
+};
 
 export const addVideo = lessonId => {
   var updates = {};
-  updates['/lessons/' + lessonId + '/video'] = {
+  updates["/lessons/" + lessonId + "/video"] = {
     src: false,
     title: false
   };
@@ -150,67 +178,69 @@ export const addVideo = lessonId => {
   return {
     type: c.ADD_VIDEO,
     lessonId
-  }
-}
+  };
+};
 
-export const updateVideo = (params) => {
+export const updateVideo = params => {
   const { url, title, lessonId } = params;
   var updates = {};
-  updates['/lessons/' + lessonId + '/video'] = {
+  updates["/lessons/" + lessonId + "/video"] = {
     src: url,
     title
   };
   db.ref().update(updates);
   return { type: c.UPDATE_VIDEO, src: url, title, lessonId };
-}
+};
 
 export const deleteVideo = lessonId => {
   var updates = {};
-  updates['/lessons/' + lessonId + '/video'] = {};
+  updates["/lessons/" + lessonId + "/video"] = {};
   db.ref().update(updates);
   return {
     type: c.REMOVE_VIDEO,
     lessonId
-  }
-}
+  };
+};
 
 export const addPicture = params => {
   const { lessonId, allContent } = params;
   const newPicture = {
-    type: 'picture',
+    type: "picture",
     mode: false,
     pictures: false
-  }
+  };
   const newLessonContent = [...allContent, newPicture];
   var updates = {};
-  updates['/lessons/' + lessonId + '/content'] = newLessonContent;
+  updates["/lessons/" + lessonId + "/content"] = newLessonContent;
   db.ref().update(updates);
   return { type: c.ADD_PICTURE, lessonId, newPicture };
-}
+};
 
-export const updatePicture = (params) => {
+export const updatePicture = params => {
   const { url, alt, lessonId, elementIndex } = params;
-  if (!url) return 'NO URL!!!'
+  if (!url) return "NO URL!!!";
   const updatedPicture = {
-    pictures: [{
-      src: url,
-      alt
-    }],
+    pictures: [
+      {
+        src: url,
+        alt
+      }
+    ],
     mode: 1,
-    type: 'picture'
+    type: "picture"
   };
   var updates = {};
-  updates['/lessons/' + lessonId + '/content/' + elementIndex] = updatedPicture;
+  updates["/lessons/" + lessonId + "/content/" + elementIndex] = updatedPicture;
   db.ref().update(updates);
   return { type: c.UPDATE_PICTURE, lessonId, elementIndex, updatedPicture };
-}
+};
 
 export const deleteText = params => {
   const { elementIndex, lessonId, fullLessonContent } = params;
   const newLessonContent = [...fullLessonContent];
   newLessonContent.splice(elementIndex, 1);
   var updates = {};
-  updates['/lessons/' + lessonId + '/content'] = newLessonContent;
+  updates["/lessons/" + lessonId + "/content"] = newLessonContent;
   db.ref().update(updates);
   return {
     type: c.REMOVE_ELEMENT,
@@ -226,7 +256,7 @@ export const moveElement = params => {
   const after = newContent.slice(elementIndex + 1);
   const element = newContent[elementIndex];
   let move;
-  if (direction === 'down') {
+  if (direction === "down") {
     move = after.shift();
     newContent = [...before, move, element, ...after];
   } else {
@@ -234,15 +264,12 @@ export const moveElement = params => {
     newContent = [...before, element, move, ...after];
   }
   var updates = {};
-  updates['/lessons/' + lessonId + '/content'] = newContent;
+  updates["/lessons/" + lessonId + "/content"] = newContent;
   db.ref().update(updates);
   return {
-    type: c.MOVE_ELEMENT, elementIndex: elementIndex,
-    lessonId: lessonId, direction: direction
-  }
-}
-
-
-
-
-
+    type: c.MOVE_ELEMENT,
+    elementIndex: elementIndex,
+    lessonId: lessonId,
+    direction: direction
+  };
+};
