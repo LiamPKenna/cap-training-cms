4dd9c2cb940f2ebb28252ac0d9fabf8ac0bc9329
LiamPKenna <liampkenna@gmail.com>
2020-03-09 16:27:07 -0700

    Add case for creating a new user

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,4 +1,4 @@
-import constants from '../constants';
+import constants from "../constants";
 
 const c = constants.c;
 
@@ -9,9 +9,15 @@ const userReducer = (state = { admin: false }, action) => {
     case c.SET_ADMIN:
       newState.admin = action.admin;
       return newState;
+    case c.ADD_USER:
+      newState[action.email.split(".").join("")] = {
+        email: action.email,
+        completedCourses: action.completedCourses
+      };
+      return newState;
     default:
       return state;
-  };
-}
+  }
+};
 
 export default userReducer;
