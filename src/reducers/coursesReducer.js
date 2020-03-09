a46654fc7a96b324abfc8aa88338a34d281344b0
LiamPKenna <liampkenna@gmail.com>
2020-03-09 15:37:10 -0700

    Add cases for removing and renaming courses

--- src/reducers/coursesReducer.js
+++ src/reducers/coursesReducer.js
@@ -1,44 +1,56 @@
-import constants from '../constants';
+import constants from "../constants";
 const c = constants.c;
 
 const initialState = {};
 const coursesReducer = (state = initialState, action) => {
   const { type } = action;
-  let newState;
+  let newState = { ...state };
   let newCourse;
   let newSegments;
   let newSegment;
   let newLessons;
   switch (type) {
-    case 'UPDATE':
+    case "UPDATE":
       console.log(action);
-
 
       break;
     case c.RECEIVE_COURSE:
-      newState = Object.assign({}, state);
       newState[action.course.id] = action.course;
       return newState;
     case c.ADD_SEGMENT:
-      newState = Object.assign({}, state);
       newCourse = { ...newState[action.courseId] };
       newSegments = { ...newCourse.segments };
       newSegments[action.segmentId] = {
         title: action.title,
-        lessons: action.lessons,
+        lessons: action.lessons
       };
-      newCourse.segments = newSegments
-      newState[action.courseId] = newCourse
+      newCourse.segments = newSegments;
+      newState[action.courseId] = newCourse;
       return newState;
     case c.ADD_LESSON_TO_SEGMENT:
-      newState = Object.assign({}, state);
       newCourse = { ...newState[action.courseId] };
       newSegments = { ...newCourse.segments };
       newSegment = newSegments[action.segmentId];
-      newLessons = [...newSegment.lessons, { title: action.title, lessonId: action.lessonId }];
+      newLessons = [
+        ...newSegment.lessons,
+        { title: action.title, lessonId: action.lessonId }
+      ];
       newSegment.lessons = newLessons;
       newSegments[action.segmentId] = newSegment;
       newCourse.segments = newSegments;
+      newState[action.courseId] = newCourse;
+      return newState;
+    case c.REMOVE_COURSE:
+      const newerState = {};
+      Object.keys(newState).forEach(course => {
+        if (course !== action.courseId) {
+          newerState[course] = newState[course];
+        }
+      });
+      return newerState;
+    case c.UPDATE_COURSE_TITLE:
+      newCourse = { ...newState[action.courseId] };
+      newCourse.title = action.name;
       newState[action.courseId] = newCourse;
       return newState;
     default:
@@ -46,6 +58,6 @@ const coursesReducer = (state = initialState, action) => {
   }
 
   return state;
-}
+};
 
 export default coursesReducer;
