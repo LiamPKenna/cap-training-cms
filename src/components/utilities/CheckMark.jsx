87518da12d0cb6e3173e5533fc82ecd8ef989c27
LiamPKenna <liampkenna@gmail.com>
2020-03-10 11:11:52 -0700

    Add CheckMark component

---
+++ src/components/utilities/CheckMark.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import DoneOutlineIcon from "@material-ui/icons/DoneOutline";
+
+const CheckDiv = styled.div`
+  color: lightgreen;
+  margin-left: 1rem;
+  display: inline;
+  font-size: 1rem;
+`;
+
+const CheckMark = () => {
+  return (
+    <CheckDiv>
+      <DoneOutlineIcon fontSize="small" />
+    </CheckDiv>
+  );
+};
+
+export default CheckMark;
