902a9273bc6170755b50c60f39751c285545f307
LiamPKenna <liampkenna@gmail.com>
2020-03-12 06:38:27 -0700

    Change db.ref statements to point at new folder

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ const { c } = constants;
 
 export function watchFirebaseLessonsRef() {
   return dispatch => {
-    db.ref('/lessons').on('child_added', params => {
+    db.ref('/epi/lessons').on('child_added', params => {
       const newLesson = Object.assign({}, params.val());
       dispatch(receiveLesson(newLesson));
     });
@@ -20,7 +20,7 @@ export const receiveLesson = lessonFromFirebase => {
 
 export function watchFirebaseCoursesRef() {
   return dispatch => {
-    db.ref('/courses').on('child_added', params => {
+    db.ref('/epi/courses').on('child_added', params => {
       const newCourse = Object.assign({}, params.val());
       dispatch(receiveCourse(newCourse));
     });
@@ -40,7 +40,7 @@ export const addUser = email => {
     email,
     completedLessons: { default: true },
   };
-  db.ref().update(updates);
+  db.ref('/epi').update(updates);
   return {
     type: c.ADD_USER,
     completedLessons: { default: true },
@@ -56,12 +56,12 @@ export const updateText = params => {
   var updates = {};
   updates[
     '/lessons/' +
-      params.lessonId +
-      '/content/' +
-      params.elementIndex +
-      '/content'
+    params.lessonId +
+    '/content/' +
+    params.elementIndex +
+    '/content'
   ] = params.content;
-  db.ref().update(updates);
+  db.ref('/epi').update(updates);
   return {
     type: c.UPDATE_TEXT,
     elementIndex: params.elementIndex,
@@ -72,7 +72,7 @@ export const updateText = params => {
 
 export const addCourse = (courseTitle = 'New Course') => {
   const newCourseKey = db
-    .ref()
+    .ref('/epi/')
     .child('courses')
     .push().key;
   var updates = {};
@@ -86,7 +86,7 @@ export const addCourse = (courseTitle = 'New Course') => {
     title: courseTitle,
     id: newCourseKey,
   };
-  db.ref().update(updates);
+  db.ref('/epi').update(updates);
   return {
     type: c.ADD_COURSE,
     title: courseTitle,
@@ -98,7 +98,7 @@ export const updateCourseName = params => {
   const { courseId, name } = params;
   const updates = {};
   updates['/courses/' + courseId + '/title'] = name;
-  db.ref().update(updates);
+  db.ref('/epi').update(updates);
   return { type: c.UPDATE_COURSE_TITLE, courseId, name };
 };
 
@@ -113,7 +113,7 @@ export const deleteCourse = params => {
     updates['/lessons/' + l.lessonId] = {};
   });
   updates['/courses/' + courseId] = {};
-  db.ref().update(updates);
+  db.ref('/epi').update(updates);
   const lessonsToDelete = lessons.map(l => ({
     type: c.REMOVE_LESSON,
     lessonId: l.lessonId,
@@ -123,7 +123,7 @@ export const deleteCourse = params => {
 
 export const addSegment = (segmentTitle = 'New segment', courseId) => {
   const newSegmentKey = db
-    .ref('/courses/' + courseId)
+    .ref('/epi/courses/' + courseId)
     .child('segments')
     .push().key;
   var updates = {};
@@ -131,7 +131,7 @@ export const addSegment = (segmentTitle = 'New segment', courseId) => {
     lessons: [{ lessonId: 0, title: 'default' }],
     title: segmentTitle,
   };
-  db.ref().update(updates);
+  db.ref('/epi').update(updates);
   return {
     type: c.ADD_SEGMENT,
     title: segmentTitle,
@@ -148,7 +148,7 @@ export const newLesson = (
   oldLessons
 ) => {
   const newLessonKey = db
-    .ref()
+    .ref('/epi')
     .child('lessons')
     .push().key;
   var updates = {};
@@ -166,7 +166,7 @@ export const newLesson = (
       lessonId: newLessonKey,
     },
   ];
-  db.ref().update(updates);
+  db.ref('/epi').update(updates);
   return [
     {
       type: c.ADD_LESSON,
@@ -190,15 +190,15 @@ export const createElement = (type, oldContent, lessonId) => {
     type === 'break'
       ? { type }
       : {
-          type,
-          content: `This is a new ${type.toUpperCase()} element`,
-          format: {
-            align: 'left',
-          },
-        };
+        type,
+        content: `This is a new ${type.toUpperCase()} element`,
+        format: {
+          align: 'left',
+        },
+      };
   var updates = {};
   updates['/lessons/' + lessonId + '/content'] = [...oldContent, newElement];
-  db.ref().update(updates);
+  db.ref('/epi').update(updates);
   return {
     type: c.ADD_ELEMENT,
     lessonId,
@@ -212,7 +212,7 @@ export const addVideo = lessonId => {
     src: false,
     title: false,
   };
-  db.ref().update(updates);
+  db.ref('/epi').update(updates);
   return {
     type: c.ADD_VIDEO,
     lessonId,
@@ -226,14 +226,14 @@ export const updateVideo = params => {
     src: url,
     title,
   };
-  db.ref().update(updates);
+  db.ref('/epi').update(updates);
   return { type: c.UPDATE_VIDEO, src: url, title, lessonId };
 };
 
 export const deleteVideo = lessonId => {
   var updates = {};
   updates['/lessons/' + lessonId + '/video'] = {};
-  db.ref().update(updates);
+  db.ref('/epi').update(updates);
   return {
     type: c.REMOVE_VIDEO,
     lessonId,
@@ -250,7 +250,7 @@ export const addPicture = params => {
   const newLessonContent = [...allContent, newPicture];
   var updates = {};
   updates['/lessons/' + lessonId + '/content'] = newLessonContent;
-  db.ref().update(updates);
+  db.ref('/epi').update(updates);
   return { type: c.ADD_PICTURE, lessonId, newPicture };
 };
 
@@ -269,7 +269,7 @@ export const updatePicture = params => {
   };
   var updates = {};
   updates['/lessons/' + lessonId + '/content/' + elementIndex] = updatedPicture;
-  db.ref().update(updates);
+  db.ref('/epi').update(updates);
   return { type: c.UPDATE_PICTURE, lessonId, elementIndex, updatedPicture };
 };
 
@@ -279,7 +279,7 @@ export const deleteText = params => {
   newLessonContent.splice(elementIndex, 1);
   var updates = {};
   updates['/lessons/' + lessonId + '/content'] = newLessonContent;
-  db.ref().update(updates);
+  db.ref('/epi').update(updates);
   return {
     type: c.REMOVE_ELEMENT,
     lessonId,
@@ -303,7 +303,7 @@ export const moveElement = params => {
   }
   const updates = {};
   updates['/lessons/' + lessonId + '/content'] = newContent;
-  db.ref().update(updates);
+  db.ref('/epi').update(updates);
   return {
     type: c.MOVE_ELEMENT,
     elementIndex: elementIndex,
@@ -317,14 +317,14 @@ export const lessonCompleted = params => {
   updates[
     '/users/' + params.currentUser + '/completedLessons/' + params.lessonId
   ] = true;
-  db.ref().update(updates);
+  db.ref('/epi').update(updates);
   return getCompletedLessons(params.currentUser);
 };
 
 export const getCompletedLessons = currentUser => {
   return async dispatch => {
     return db
-      .ref('/users/' + currentUser)
+      .ref('/epi/users/' + currentUser)
       .once('value')
       .then(snap => {
         if (snap.val()) {
@@ -342,7 +342,7 @@ export const getCompletedLessons = currentUser => {
 export const getAllUsers = params => {
   return async dispatch => {
     return db
-      .ref('/users')
+      .ref('/epi/users')
       .once('value')
       .then(snap => {
         if (snap.val()) {
