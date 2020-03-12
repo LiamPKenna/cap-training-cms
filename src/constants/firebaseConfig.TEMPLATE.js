f7fe1b1830080b995e334a34f9f5aad6fb35f01e
Student <example@example.com>
2020-03-12 10:18:07 -0700

    Add template file for firebase connection config

---
+++ src/constants/firebaseConfig.TEMPLATE.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: { YOUR_KEY },
+  authDomain: { YOUR_DOMAIN },
+  databaseURL: { YOUR_URL },
+  projectId: { YOUR_ID },
+  storageBucket: { YOUR_BUCKET },
+  messagingSenderId: { YOUR_SENDER_ID },
+  appId: { YOUR_APP_ID },
+  measurementId: { YOUR_MEASUREMENT_ID },
+};
+
+export default firebaseConfig;
