9d090c03313afd768bf93f29768e7f8930299057
Student <example@example.com>
2020-03-12 11:06:58 -0700

    Remove empty line

--- src/constants/mockData/mockBrand.js
+++ src/constants/mockData/mockBrand.js
@@ -1,7 +1,7 @@
 const mockBrand = {
   name: 'Epicodus',
-  logo: 'https://static1.squarespace.com/static/5524448ee4b0d6f6b83ab9e2/t/57cf3de246c3c4d2933aa57c/1529949737992/?format=1500w'
-
+  logo:
+    'https://static1.squarespace.com/static/5524448ee4b0d6f6b83ab9e2/t/57cf3de246c3c4d2933aa57c/1529949737992/?format=1500w',
 };
 
 export default mockBrand;
