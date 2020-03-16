cdd6c09bc6668107c4110b5197b63578acc24334
LiamPKenna <liampkenna@gmail.com>
2020-03-16 07:56:20 -0700

    Update image url

--- src/constants/mockData/mockBrand.js
+++ src/constants/mockData/mockBrand.js
@@ -3,7 +3,7 @@ const mockBrand = {
   // logo:
   //   'https://static1.squarespace.com/static/5524448ee4b0d6f6b83ab9e2/t/57cf3de246c3c4d2933aa57c/1529949737992/?format=1500w',
   name: "CAP Training CMS",
-  logo: process.env.PUBLIC_URL + 'logo512.png'
+  logo: 'https://firebasestorage.googleapis.com/v0/b/cap-training-cms.appspot.com/o/images%2Flogo512.png?alt=media&token=b9f555d1-a848-44d0-aa1e-d5ca9b998e6e'
 };
 
 export default mockBrand;
