d1239760d935e0217ec8540a6da235da655c4c17
LiamPKenna <liampkenna@gmail.com>
2020-03-16 06:34:03 -0700

    Update styles and mockBrand

--- src/constants/mockData/mockBrand.js
+++ src/constants/mockData/mockBrand.js
@@ -1,7 +1,9 @@
 const mockBrand = {
-  name: 'Epicodus',
-  logo:
-    'https://static1.squarespace.com/static/5524448ee4b0d6f6b83ab9e2/t/57cf3de246c3c4d2933aa57c/1529949737992/?format=1500w',
+  // name: 'Epicodus',
+  // logo:
+  //   'https://static1.squarespace.com/static/5524448ee4b0d6f6b83ab9e2/t/57cf3de246c3c4d2933aa57c/1529949737992/?format=1500w',
+  name: "CAP Training CMS",
+  logo: process.env.PUBLIC_URL + 'logo512.png'
 };
 
 export default mockBrand;
